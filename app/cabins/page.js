78aa63af15087f3f9179f9428a82236330f0af46
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-09-30 17:52:24 +0300

    Edited project organization

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,4 +1,4 @@
-import Counter from "../components/Counter";
+import Counter from "./Counter";
 
 export default async function Page() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
