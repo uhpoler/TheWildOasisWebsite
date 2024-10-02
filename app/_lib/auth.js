b806e69bd1217cbf0bc7ad6c0ce04937fb240bea
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-10-02 12:55:01 +0300

    Set up NextAuth

---
+++ app/_lib/auth.js
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+const authConfig = {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+};
+
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth(authConfig);
